6bbff6812b261c7657af2bfe645499692bedb54a
Hai <haiboy@hotmail.nl>
2020-10-06 09:06:58 +0200

    Visualizations

--- Race Simulator/Visualization.cs
+++ Race Simulator/Visualization.cs
@@ -1,16 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Race_Simulator
+﻿namespace Race_Simulator
 {
     public static class Visualization
     {
         public static void Initalize()
         {
-
+            System.Console.WriteLine(_finishHorizontal);
         }
         #region graphics
+        private static string[] _finishHorizontal =
+        {
+            "----",
+            "  # ",
+            "  # ",
+            "----"
+        };
+        private static string[] _straightHorizontal =
+{
+            "----",
+            "    ",
+            "    ",
+            "----"
+        };
+        private static string[] _straightVertical =
+{
+            "|  |",
+            "|  |",
+            "|  |",
+            "|  |"
+        };
+        private static string[] _bendSW =
+        {
+            "--\\ ",
+            "   \\",
+            "\\  |",
+            "|  |"
+        };
+        private static string[] _bendSE =
+        {
+            " /--",
+            "/   ",
+            "|   /",
+            "|   |"
+        };
+        private static string[] _bendNW =
+        {
+            "   ",
+            "",
+            "",
+            ""
+        };
+
         #endregion
     }
 }
